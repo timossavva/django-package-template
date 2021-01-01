93e8187d0200602c86210ae3b1acf15fa18432f7
timos222 <timo.savva.ebay@gmail.com>
2021-01-01 19:53:39 +0200

    fhm

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,16 +5,15 @@ from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 
 from django_project import settings
-from django_project.settings import PREFIX_DEFAULT_LANGUAGE
 
 urlpatterns = i18n_patterns(
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-    prefix_default_language=False
+    path('admin/', admin.site.urls),
+    # path('', include('my_django_app.urls')),
+    # prefix_default_language=False
 )
 
 urlpatterns += [
-    path('admin/', admin.site.urls),
-    path('config-dyna-builder/', include('config_dyna_builder.urls')),
     # Django's 'set_language' view, for the language switcher redirection
     path('i18n/', include('django.conf.urls.i18n')),
 ]
